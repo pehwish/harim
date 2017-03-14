ca8679060926d95b527ff64087dc5234c716b58c
FE-PC\FE <cizel444@gmail.com>
2017-03-14 20:24:42 +0900

    메인페이지 작업

--- webapp/_resource/js/common.js
+++ webapp/_resource/js/common.js
@@ -1,4 +1,34 @@
 $(document).ready(function(){
+	/* GNB */
+	$(".mainMenuGroup a").on("mouseenter", function(){
+		$("#gnb").addClass("active");
+	});
+	$(".mainMenuGroup, #gnb").on("mouseleave", function(){
+		var gnbActive = $("#gnb").hasClass("active");
+		if(!gnbActive || $(this).attr("id") == "gnb"){
+			$("#gnb").removeClass("active");
+		}
+	});
+
+	/* BG IMAGE */
+	var body = $('.bgAction');
+	var backgrounds = new Array(
+		"url(../../_resource/images/common/bg_main01.jpg) no-repeat",
+		"url(../../_resource/images/common/bg_main02.jpg) no-repeat"
+	);
+	var current = 0;
+	 function nextBackground() {
+		body.fadeOut(1000);
+		body.css(
+			'background',
+			backgrounds[current = ++current % backgrounds.length]
+		);
+		body.fadeIn(1000);
+		setTimeout(nextBackground, 3000);
+	}
+	setTimeout(nextBackground, 3000);
+	body.css('background', backgrounds[0]);
+ 
 	/* sub menu tab action */
 	$(".subLinkGroup a").on("mouseover", function(){
 		$(".subLinkGroup a.active").addClass("holder");
@@ -26,5 +56,20 @@ $(document).ready(function(){
 	});
 	$("._select").on("mouseleave", function(){
 		$(this).find("dd").hide();
+	});
+
+	/* bxslider */
+	$("#mainSliderCompany").bxSlider({
+		auto:true,
+		pager:false,
+	});
+	$('.bannerBox').bxSlider({
+		auto:true,
+		pager:false,
+		slideWidth:135,
+		minSlides:2,
+		maxSlides:7,
+		moveSlides:1,
+		slideMargin:0
 	});
 });
